dacb0cfc74943232af12c866f8d5fba9aa080e69
Miguel Savignano <migue.masx@gmail.com>
2016-08-28 13:41:24 +0200

    ok

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,8 @@
 class SiteController < ApplicationController
+  include ReactOnRails::Controller
   def react_demo
     @react_component = "Demo"
     @react_props = {}
+    redux_store "Store", props:{max_number:10}
   end
 end
