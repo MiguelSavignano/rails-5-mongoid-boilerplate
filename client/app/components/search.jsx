80a9d2e3e6d0f7c5f4fbe92765737d5e0b6603e3
Miguel Savignano <migue.masx@gmail.com>
2016-08-30 23:33:28 +0200

    search ok

--- client/app/components/search.jsx
+++ client/app/components/search.jsx
@@ -4,21 +4,26 @@ import {observer}           from "mobx-react"
 import _                    from 'lodash'
 
 export default @observer class Search extends React.Component {
+  @observable users_filterd = this.props.users
   @observable users = this.props.users
+  onSeach(event){
+    let searchValue = event.target.value
+    this.users_filterd = this.users.filter( (user) => (
+      (new RegExp(searchValue, "i") ).test(user.name)
+    ))
+  }
   render() {
     return(
-      <div>{
-        this.users.map((user)=>(
-          <div key={user.id} >
-            {user.name}
-          </div>
-        ))
-      }</div>
+      <div>
+        <input onKeyUp={this.onSeach.bind(this)} />
+        <ul>{
+          this.users_filterd.map((user)=>(
+            <li key={user.id} >
+              {user.name}
+            </li>
+          ))
+        }</ul>
+      </div>
     )
   }
 }
-        // <ul>
-        //   <li>{this.user_selected.name}</li>
-        //   <li>{this.user_selected.address}</li>
-        //   <li>{this.user_selected.id}</li>
-        // </ul>
