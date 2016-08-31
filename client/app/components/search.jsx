c78c9cd45a5aaf42f3db0f8749706495b77d7b25
Miguel Savignano <migue.masx@gmail.com>
2016-08-31 21:38:38 +0200

    use a global for set the  function

--- client/app/components/search.jsx
+++ client/app/components/search.jsx
@@ -12,8 +12,7 @@ const SearchInput = (props) =>(
 @observer class Search extends React.Component {
   constructor(props){
     super(props)
-    ReactDOM.render( <SearchInput onKeyUp={this.onSearch.bind(this)} />,
-      document.getElementById("react-SearchInput") )
+    $.Seach={ onSearch: this.onSearch.bind(this) }
   }
   @observable users_filterd = this.props.users
   @observable users = this.props.users
